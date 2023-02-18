c922d1a1be318a400b7eab4e2e532a8987849b1f
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-02-18 23:51:00 +0600

    adding function of tacking input & calcuaion of trinangle

--- js/style.js
+++ js/style.js
@@ -1,1 +1,25 @@
+//Take input value from by the function
+function get_input(idName){
+    const inputElement = document.getElementById(idName);
+    const input = parseFloat(inputElement.value);
+    console.log(input);
+
+    return input;
+}
+// Find area of Triangle , Rhombus , Pentagon
+function getTriRomPen(input1,input2){
+    const area = 0.5 * input1 * input2;
+    return area;
+}
+
 console.log('hello js');
+document.getElementById('tri-area-calculator').addEventListener("click",function(){
+    console.log('hello cal');
+    
+    const triBase = get_input('tri-base');
+    const triHeight = get_input('tri-height');
+    
+    triArea = getTriRomPen(triBase,triHeight);
+    console.log("Triangle area by Function = " + triArea);
+
+});
