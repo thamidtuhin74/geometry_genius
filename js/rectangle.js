cb90d4aa3ff128d16e21f31edcbf0082c3fb1364
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-02-19 14:34:40 +0600

    adding Resctangle & Parallelogram

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,25 +1,8 @@
-function get_input(idName){
-    const inputElement = document.getElementById(idName);
-    const input = parseFloat(inputElement.value);
-    console.log(input);
-
-    return input;
-}
-// Find area of Ractangle , Para
-function getRecPara(input1,input2){
-    const area = input1 * input2;
-    return area;
-    }
-
-function displayCalculatedArea( shapeName, Area){
-
-    document.getElementById(shapeName).style.display = 'flex';
-    document.getElementById('shape-name').innerHTML = shapeName;
-    document.getElementById('area-value').innerHTML = Area;
-
-}
-
-
+// Find area of Rectangle , Parallelogram
+// function getRecPara(input1,input2){
+//     const area = input1 * input2;
+//     return area;
+//     }
 console.log('hello Rec------>');
 
 // function getRecPara(input1,input2){
@@ -27,15 +10,15 @@ console.log('hello Rec------>');
 //     return area;
 // }
 
-document.getElementById('rac-area-calculator').addEventListener("click",function(){
-    console.log('hello Rec');
+// document.getElementById('rec-area-calculator').addEventListener("click",function(){
+//     console.log('hello Rec');
     
-    const recWidth = get_input('rec-width');
-    const recLength = get_input('rec-length');
+//     const recWidth = get_input('rec-width');
+//     const recLength = get_input('rec-length');
     
-    recArea = getTriRomPen(recWidth,recLength);
-    console.log("Triangle area by Function = " + recArea);
+//     recArea = getRecPara(recWidth,recLength);
+//     console.log("Rectangle area by Function = " + recArea);
     
-    displayCalculatedArea('rectangle',recArea);
+//     displayCalculatedArea('rectangle',recArea);
 
-});
+// });
--- js/style.js
+++ js/style.js
@@ -11,17 +11,22 @@ function getTriRomPen(input1,input2){
     const area = 0.5 * input1 * input2;
     return area;
 }
+// Find area of Rectangle , Parallelogram
+function getRecPara(input1,input2){
+    const area = input1 * input2;
+    return area;
+    }
+
 function displayCalculatedArea( shapeName, Area){
 
     document.getElementById(shapeName).style.display = 'flex';
-    document.getElementById('shape-name').innerHTML = shapeName;
-    document.getElementById('area-value').innerHTML = Area;
+    // document.getElementById(shapeName+'shape-name').innerHTML = shapeName;
+    document.getElementById(shapeName+'area-value').innerHTML = Area;
 
 }
 
-console.log('hello js');
+// Triangle Functions
 document.getElementById('tri-area-calculator').addEventListener("click",function(){
-    console.log('hello cal');
     
     const triBase = get_input('tri-base');
     const triHeight = get_input('tri-height');
@@ -35,23 +40,33 @@ document.getElementById('tri-area-calculator').addEventListener("click",function
 
 });
 
-// function getRecPara(input1,input2){
-//     const area = input1 * input2;
-//     return area;
-// }
+ // Rectangle Functions
 
-// document.getElementById('rac-area-calculator').addEventListener("click",function(){
-//     console.log('hello Rec');
+document.getElementById('rec-area-calculator').addEventListener("click",function(){
     
-//     const recWidth = get_input('rec-width');
-//     const recLength = get_input('rec-length');
+    const recWidth = get_input('rec-width');
+    const recLength = get_input('rec-length');
     
-//     recArea = getTriRomPen(recWidth,recLength);
-//     console.log("Triangle area by Function = " + recArea);
+    recArea = getRecPara(recWidth,recLength);
+    console.log("Rectangle area by Function = " + recArea);
     
-//     displayCalculatedArea('rectangle',recArea);
+    displayCalculatedArea('rectangle',recArea);
 
-// });
+});
+
+// Rectangle Functions
+
+document.getElementById('para-area-calculator').addEventListener("click",function(){
+    
+    const paraWidth = get_input('para-width');
+    const paraLength = get_input('para-length');
+    
+    paraArea = getRecPara(paraWidth,paraLength);
+    console.log("Parallelogram area by Function = " + paraArea);
+    
+    displayCalculatedArea('parallelogram',paraArea);
+
+});
 
     
     
